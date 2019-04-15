9585111f1daf3615e1c7ea52d516d5323e65e6df
Kamil Piszczek <k4mil.piszczek@gmail.com>
2019-04-15 20:13:48 +0200

    migrations: fixing deploy logic

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
 const Splitter = artifacts.require("Splitter");
 
 module.exports = function(deployer, network, accounts) {
-    deployer.then(() => {
-        return deployer.deploy(Splitter, accounts[1], accounts[2]);
-    }).catch(console.error);
+    deployer.deploy(Splitter, accounts[1], accounts[2]);
 };
