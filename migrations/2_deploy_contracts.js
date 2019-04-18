dff0ac00f8564f317ada872b79083e5aa24be738
Kamil Piszczek <k4mil.piszczek@gmail.com>
2019-04-18 18:29:18 +0200

    migrations: removing redundant deployment logic for SafeMath

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,5 @@
 const Splitter = artifacts.require("Splitter");
-const SafeMath = artifacts.require("SafeMath");
 
 module.exports = function(deployer, network, accounts) {
-    deployer.deploy(SafeMath).then(() => {
-        return deployer.link(SafeMath, Splitter);
-    }).then(() => {
-        return deployer.deploy(Splitter, accounts[1], accounts[2]);
-    });
+    deployer.deploy(Splitter, accounts[1], accounts[2]);
 };
